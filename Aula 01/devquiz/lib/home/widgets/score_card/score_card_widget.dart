a407bf5eb3a894086bd7345684096d2c895ecbbb
rogerioxonchim <rogerio@voti.com.br>
2021-04-20 00:28:13 -0300

    Primeira Aula

---
+++ Aula 01/devquiz/lib/home/widgets/score_card/score_card_widget.dart
@@ -0,0 +1,44 @@
+import 'package:devquiz/core/app_colors.dart';
+import 'package:devquiz/core/core.dart';
+import 'package:devquiz/home/widgets/chart/chart_widget.dart';
+import 'package:flutter/material.dart';
+
+class ScoreCardWidget extends StatelessWidget {
+  const ScoreCardWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10, right: 10),
+      child: Container(
+        height: 126,
+        decoration: BoxDecoration(
+            color: AppColors.white, borderRadius: BorderRadius.circular(20)),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(flex: 1, child: ChartWidget()),
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Vamos começar",
+                      style: AppTextStyles.heading,
+                    ),
+                    Text("Complete os desafios e avançe em conhecimento",
+                        style: AppTextStyles.body)
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
