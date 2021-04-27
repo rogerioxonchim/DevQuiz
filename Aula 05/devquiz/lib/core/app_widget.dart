c136f76da26dcd704bb0bc13ac512e89738fbfc8
rogerioxonchim <rogerio@voti.com.br>
2021-04-26 21:12:03 -0300

    Aula 05

---
+++ Aula 05/devquiz/lib/core/app_widget.dart
@@ -0,0 +1,13 @@
+import 'package:devquiz/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "DevQuiz",
+      //home: ChallengePage(),
+      home: SplashPage(),
+    );
+  }
+}
---
+++ Aula 05/devquiz/lib/home/home_page.dart
@@ -0,0 +1,109 @@
+import 'package:devquiz/challenge/challenge_page.dart';
+import 'package:devquiz/core/app_colors.dart';
+import 'package:devquiz/home/home_controller.dart';
+import 'package:devquiz/home/home_state.dart';
+import 'package:devquiz/home/widgets/app_bar_widget.dart';
+import 'package:devquiz/home/widgets/quiz_card/quiz_card_widget.dart';
+import 'package:flutter/material.dart';
+import 'widgets/level_button/level_button_widget.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.getUSer();
+    controller.getQuizzes();
+    controller.stateNotifier.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (controller.user != null && controller.quizzes != null) {
+      if (controller.state == HomeState.sucess) {
+        return Scaffold(
+            appBar: AppBarWidget(user: controller.user!),
+            body: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  SizedBox(height: 24),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      LevelButtonWidget(
+                        label: "Fácil",
+                      ),
+                      LevelButtonWidget(
+                        label: "Médio",
+                      ),
+                      LevelButtonWidget(
+                        label: "Difícil",
+                      ),
+                      LevelButtonWidget(
+                        label: "Perito",
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 24),
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisSpacing: 16,
+                      mainAxisSpacing: 16,
+                      crossAxisCount: 2,
+                      children: controller.quizzes!
+                          .map((e) => QuizCardWidget(
+                                title: e.title,
+                                percent:
+                                    e.questionAnswered / e.questions.length,
+                                completed:
+                                    "${e.questionAnswered}/${e.questions.length}",
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => ChallengePage(
+                                                questions: e.questions,
+                                                title: e.title,
+                                              )));
+                                },
+                              ))
+                          .toList(),
+                    ),
+                  )
+                ],
+              ),
+            ));
+      } else {
+        return Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(AppColors.darkGreen),
+            ),
+          ),
+        );
+      }
+    } else {
+      controller.getUSer();
+      controller.getQuizzes();
+
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(AppColors.darkGreen),
+          ),
+        ),
+      );
+    }
+  }
+}
